a6e4fb7090b85de770c2c8fb54e30e8b774a887c
Blaine Mooers <bmooers1@gmail.com>
2021-01-28 17:23:21 -0600

    Added CommandlineTools updated.

--- CommandLineTools/updateCLT.py
+++ CommandLineTools/updateCLT.py
@@ -12,3 +12,38 @@ sudo rm /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
 # Description:  Update the command line tools for Xcode on Mac OS X. 
 # Source:  NA
 
+sudo touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+softwareupdate -l
+# Update command line tools via software update.
+sudo rm /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+# Description:  Update the command line tools for Xcode on Mac OS X. 
+# Source:  NA
+
+sudo touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+softwareupdate -l
+# Update command line tools via software update.
+sudo rm /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+# Description:  Update the command line tools for Xcode on Mac OS X. 
+# Source:  NA
+
+sudo touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+softwareupdate -l
+# Update command line tools via software update.
+sudo rm /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+# Description:  Update the command line tools for Xcode on Mac OS X. 
+# Source:  NA
+
+sudo touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+softwareupdate -l
+# Update command line tools via software update.
+sudo rm /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+# Description:  Update the command line tools for Xcode on Mac OS X. 
+# Source:  NA
+
+sudo touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+softwareupdate -l
+# Update command line tools via software update.
+sudo rm /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+# Description:  Update the command line tools for Xcode on Mac OS X. 
+# Source:  NA
+
